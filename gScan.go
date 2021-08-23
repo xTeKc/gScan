b79cba6d65007fca37cb509596f49684c49addf2
xTeKc <bcd3v@pm.me>
2021-08-22 23:22:42 -0400

    refactor

--- gScan.go
+++ gScan.go
@@ -28,5 +28,35 @@ func portScan(ip string, port string, wg *sync.WaitGroup) {
 }
 
 func main() {
+	//get arg for ip
+	ip := flag.string("h", "", "Select IP to Scan")
+	//parse arg
+	flag.Parse()
+		//slice stores all 65536 ports
+		var prt []int
+		//slice sorts all 65536 ports and converts to string
+		prtStr := []string{}
+		//integer slices with 65536 slices
+		allP := make([]int, 65536)
+		//iterate all 65536 slices and append to prt
+		for p := range allP {
+			prt = append(prt, p)
+		}
+		//convert int slice into string slice
+		for i := range prt {
+			n := prt[i]
+			text := strconv.Itoa(n)
+			prtStr = append(prtStr, text)
+		}
 	
+		var wg sync.WaitGroup
+	
+		for _, p := range prtStr {
+			//if counter equals zero, all go routines blocked on Wait are released
+			wg.Add(1)
+			//call portScan function and iterate through every port on ip address concurrently
+			go portScan(*ip, p, &wg)
+		}
+	
+		wg.Wait()	
 }
