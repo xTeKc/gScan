be00b4280774d54d376f20882addd0b35c8e0152
xTeKc <bcd3v@pm.me>
2021-08-24 21:42:47 -0400

    add waitgroup to func

--- ncgScan.go
+++ ncgScan.go
@@ -7,8 +7,11 @@ import (
 )
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 1; i <= 1024; i++ {
+		wg.Add(1)
 		go func(j int) {
+			defer wg.Done()
 			address := fmt.Sprintf("scanme.nmap.org:%d", j)
 			conn, err := net.Dial("tcp", address)
 			if err != nil {
@@ -18,4 +21,5 @@ func main() {
 			fmt.Printf("%d open\n", j)
 		}(i)
 	}
+	wg.Wait()
 }
