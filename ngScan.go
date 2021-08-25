7ae65e2cb83715b418cde7c7e30b5239bbb44605
xTeKc <bcd3v@pm.me>
2021-08-24 21:33:02 -0400

    add a refactored func version

--- ngScan.go
+++ ngScan.go
@@ -5,10 +5,23 @@ import (
 	"net"
 )
 
-func main() {
-	_, err := net.Dial("tcp", "scanme.nmap.org:80")
+// func main() {
+// 	_, err := net.Dial("tcp", "scanme.nmap.org:80")
 
-	if err == nil {
-		fmt.Println("Connection Success!")
+// 	if err == nil {
+// 		fmt.Println("Connection Success!")
+// 	}
+// }
+
+func main() {
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
+	 	if err != nil {
+			 //port is closed or filterd
+			 continue
+		}
+		conn.Close()
+		fmt.Printf("%d open\n", i)
 	}
 }
